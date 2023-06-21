3b606d3d38041ea3d429375b6f7d9c27686aab94
Stefania Alexandru <stefaniaalexandru00@gmail.com>
2023-06-21 18:29:30 +0300

    Updated unit tests

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -15,149 +15,130 @@ namespace UnitTests
         public void Setup()
         {
             people = new List<Person>()
-            {
-                new Person("John", "Doe", new DateTime(1990, 1, 1), "AB1234567890"),
-                new Person("Jane", "Smith", new DateTime(1985, 5, 10), "CD2345678901"),
-                new Person("Bob", "Johnson", new DateTime(1995, 7, 15), "EF3456789012"),
-                new Person("John", "Smith", new DateTime(1992, 3, 20), "GH4567890123"),
-                new Person("John", "Doe", new DateTime(1988, 9, 5), "IJ5678901234")
-            };
+        {
+            new Person("John", "Doe", new DateTime(1990, 1, 1), "AB1234567890"),
+            new Person("Jane", "Smith", new DateTime(1985, 5, 10), "CD2345678901"),
+            new Person("Bob", "Johnson", new DateTime(1995, 7, 15), "EF3456789012"),
+            new Person("John", "Smith", new DateTime(1992, 3, 20), "GH4567890123"),
+            new Person("John", "Doe", new DateTime(1988, 9, 5), "IJ5678901234")
+        };
 
             personRepository = new PersonRepository(people);
         }
 
-        [Test]
-        public void GetByFirstName_ExistingPersonWithUniqueFirstName_ReturnsPerson()
+        [TestCase("Jane")]
+        public void GetByFirstName_ExistingPersonWithUniqueFirstName_ReturnsPerson(string firstName)
         {
-            // Arrange
-
             // Act
-            var result = personRepository.GetByFirstName("Jane");
+            var result = personRepository.GetByFirstName(firstName);
 
             // Assert
             Assert.NotNull(result);
-            Assert.AreEqual("Jane", result[0].FirstName);
-
+            Assert.AreEqual(firstName, result[0].FirstName);
         }
 
-        [Test]
-        public void GetByFirstName_NonExistingPerson_ReturnsEmptyList()
+        [TestCase("Alice")]
+        public void GetByFirstName_NonExistingPerson_ReturnsEmptyList(string firstName)
         {
-            // Arrange
-
             // Act
-            var result = personRepository.GetByFirstName("Alice");
+            var result = personRepository.GetByFirstName(firstName);
 
             // Assert
             Assert.IsEmpty(result);
         }
-        [Test]
-        public void GetByFirstName_MultiplePeopleWithSameFirstName_ReturnsAllPeople()
-        {
-            // Arrange
 
+        [TestCase("John")]
+        public void GetByFirstName_MultiplePeopleWithSameFirstName_ReturnsAllPeople(string firstName)
+        {
             // Act
-            var result = personRepository.GetByFirstName("John");
+            var result = personRepository.GetByFirstName(firstName);
 
             // Assert
             Assert.NotNull(result);
             Assert.AreEqual(3, result.Count);
-            Assert.True(result.All(person => person.FirstName == "John"));
-
+            Assert.True(result.All(person => person.FirstName == firstName));
         }
 
-        [Test]
-        public void GetByLastName_ExistingPersonWithUniqueLastName_ReturnsPerson()
+        [TestCase("Doe")]
+        public void GetByLastName_ExistingPersonWithUniqueLastName_ReturnsPerson(string lastName)
         {
-            // Arrange
-
             // Act
-            var result = personRepository.GetByLastName("Doe");
+            var result = personRepository.GetByLastName(lastName);
 
             // Assert
             Assert.NotNull(result);
-            Assert.AreEqual("Doe", result[0].LastName);
+            Assert.AreEqual(lastName, result[0].LastName);
         }
-        [Test]
-        public void GetByLastName_MultiplePeopleWithSameLastName_ReturnsAllPeople()
-        {
-            // Arrange
 
+        [TestCase("Doe")]
+        public void GetByLastName_MultiplePeopleWithSameLastName_ReturnsAllPeople(string lastName)
+        {
             // Act
-            var result = personRepository.GetByLastName("Doe");
+            var result = personRepository.GetByLastName(lastName);
 
             // Assert
             Assert.NotNull(result);
             Assert.AreEqual(2, result.Count);
-            Assert.True(result.All(person => person.LastName == "Doe"));
+            Assert.True(result.All(person => person.LastName == lastName));
         }
 
-        [Test]
-        public void GetByLastName_NonExistingPerson_ReturnsEmptyList()
+        [TestCase("Brown")]
+        public void GetByLastName_NonExistingPerson_ReturnsEmptyList(string lastName)
         {
-            // Arrange
-
             // Act
-            var result = personRepository.GetByLastName("Brown");
+            var result = personRepository.GetByLastName(lastName);
 
             // Assert
             Assert.IsEmpty(result);
         }
 
-        [Test]
-        public void GetById_ExistingPerson_ReturnsPerson()
+        [TestCase("CD2345678901")]
+        public void GetById_ExistingPerson_ReturnsPerson(string id)
         {
-            // Arrange
-
             // Act
-            var result = personRepository.GetById("CD2345678901");
+            var result = personRepository.GetById(id);
 
             // Assert
             Assert.NotNull(result);
-            Assert.AreEqual("CD2345678901", result.ID);
+            Assert.AreEqual(id, result.ID);
         }
 
-        [Test]
-        public void GetById_NonExistingPerson_ReturnsNull()
+        [TestCase("XYZ1234567890")]
+        public void GetById_NonExistingPerson_ReturnsNull(string id)
         {
-            // Arrange
-
             // Act
-            var result = personRepository.GetById("XYZ1234567890");
+            var result = personRepository.GetById(id);
 
             // Assert
             Assert.Null(result);
         }
 
-        [Test]
-        public void Add_NewPerson_PersonAddedToList()
+        [TestCase("Alice", "Brown", "2000-03-20", "KL6789012345")]
+        public void Add_NewPerson_PersonAddedToList(string firstName, string lastName, string dateOfBirth, string id)
         {
             // Arrange
-            var newPerson = new Person("Alice", "Brown", new DateTime(2000, 3, 20), "KL6789012345");
+            var newPerson = new Person(firstName, lastName, DateTime.Parse(dateOfBirth), id);
 
             // Act
-            var result = personRepository.Add(newPerson); // result - bool type 
+            var result = personRepository.Add(newPerson);
 
             // Assert
-            Assert.True(result); // check if result = true - means that the person didn't exist in people repo and it will be added
-            Assert.Contains(newPerson, people); // check if the person was added to people list
+            Assert.True(result);
+            Assert.Contains(newPerson, people);
         }
 
-        [Test]
-        public void Add_ExistingPerson_ReturnsFalse_PersonNotAddedToList()
+        [TestCase("John", "Doe", "1990-01-01", "AB1234567890")]
+        public void Add_ExistingPerson_ReturnsFalse_PersonNotAddedToList(string firstName, string lastName, string dateOfBirth, string id)
         {
             // Arrange
-            var existingPerson = new Person("John", "Doe", new DateTime(1990, 1, 1), "AB1234567890");
+            var existingPerson = new Person(firstName, lastName, DateTime.Parse(dateOfBirth), id);
 
             // Act
             var result = personRepository.Add(existingPerson);
 
             // Assert
-            Assert.False(result); // // check if result = false - means that the person does exist in people repo and it will NOT be added
+            Assert.False(result);
         }
-
-
-
-
     }
+
 }
